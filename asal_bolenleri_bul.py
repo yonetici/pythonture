e3b2a54b67a784f97d335f4ed7e141bc4f86139f
Rıdvan BİLGİN <yonetici@users.noreply.github.com>
2020-12-30 14:22:16 +0300

    Update asal_bolenleri_bul.py

--- asal_bolenleri_bul.py
+++ asal_bolenleri_bul.py
@@ -1,15 +1,22 @@
-def asal_bolenleri_bul(no):
-    i = 2
-    asallar = []
-    cikti = set()
-    while i <= no:
-        if (no % i) == 0:
-            asallar.append(i)
-            no = no / i
+import sys
+
+
+def asalBolenleriBul(x):
+    try:
+        x = int(x)
+    except ValueError:
+        print("Invalid Input")
+        sys.exit(1)
+    if x<0:
+        print("Negatif sayı olamaz.")
+        sys.exit(1)
+    asalBolenler = []
+    for i in range(2,x):
+        if(x % i == 0):
+            asalBolenler.append(i)
+            x = x / i
         else:
             i = i + 1
-    for x in asallar:
-        cikti.add(x)
-    return cikti
+    return asalBolenler
 
-#usage print (asal_bolenleri_bul(987654))
+#usage print(asalBolenleriBul(input("Sayi: ")))
